7639f2995c9befcb428889a89a9986f344a91fd5
alex <alexey.myakinky@dev4hana.ru>
2025-02-23 01:01:46 +0100

    token refresh

--- srv/lib/MyRemoteService.js
+++ srv/lib/MyRemoteService.js
@@ -1,15 +1,15 @@
 const cds = require('@sap/cds')
 const axios = require('axios')
 
-async function _getAccessToken(dst) {
+async function fetchAuthToken(dst) {
     return await axios.post(dst.tokenurl, {
         grant_type: 'client_credentials', client_id: dst.clientid, client_secret: dst.clientsecret,
     }, {
         headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
-    }).then(r => r.data.access_token)
+    }).then(r => r.data)
 }
 
-async function _getCsrfToken(url, authToken) {
+async function getCsrfToken(url, authToken) {
     return axios.head(url, {
         headers: { 'Authorization': `Bearer ${authToken}`, 'X-CSRF-Token': 'fetch' }
     }).then(r => ({ token: r.headers["x-csrf-token"], cookies: r.headers["set-cookie"] }))
@@ -21,7 +21,7 @@ class MyRemoteService extends cds.Service {
         await super.init()
         this.middlewares = []
         this.path = ''
-        this.authToken = null
+        this.auth = {}
     }
 
     async prepareAxiosRequest(reqOptions, endpointUrl, borrowSvcCreds) {
@@ -30,13 +30,17 @@ class MyRemoteService extends cds.Service {
 
         if (!destination?.url) throw new Error('CREDENTIALS_NOT_SET')
 
-        if (!this.authToken) this.authToken = await _getAccessToken(destination)
+        const fetchRefresh = !this.auth.token || Date.now() > this.auth.expires
+        if (fetchRefresh) {
+            const { access_token: token, expires_in, scope } = await fetchAuthToken(destination)
+            this.auth = { token, scope, expires: Date.now() + 1000 * expires_in }
+        }
 
         reqOptions.baseURL = destination.url + endpointUrl
-        const csrf = await _getCsrfToken(reqOptions.baseURL, this.authToken)
+        const csrf = await getCsrfToken(reqOptions.baseURL, this.auth.token)
 
         reqOptions.headers = Object.assign(reqOptions.headers || {}, {
-            'Authorization': `Bearer ${this.authToken}`,
+            'Authorization': `Bearer ${this.auth.token}`,
             'X-CSRF-Token': csrf.token,
             'Cookie': csrf.cookies // cpi REALLY WANTS it
         })
